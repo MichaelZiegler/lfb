4635cd69aff761676867f5850c622c699d869048
MichaelZiegler <mail@michaelziegler.eu>
2021-09-28 11:54:02 +0200

    Delete LFB.tsx

--- src/assets/LFB.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="LFB" tilewidth="188" tileheight="92" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="188" height="92" source="LFB-Logo.png"/>
- </tile>
-</tileset>
